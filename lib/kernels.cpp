d4ce91efea4038e8d6d546072c4ea2a1f2313608
ChanWooY-1QBit <chanwoo.yang@1qbit.com>
2022-05-19 11:13:51 -0700

    typo fixed

--- lib/kernels.cpp
+++ lib/kernels.cpp
@@ -5,14 +5,14 @@
 
 int main() {
     einsum::EinsumDescriptor descriptor = {
-        "ab,b->c", {32,30}
+        "ab,b->c", (int[]){32,30}
     };
     std::cout << "before desc subs: " << descriptor.subscripts << "\n";
     std::cout << "before desc shape: " << descriptor.tensor_shape[0] << "\n";
 
     pybind11::bytes opaque = einsum::PackDescriptor(descriptor);
 
-    const einsum::EinsumDescriptor &convDescriptor = *UnpackDescriptor(opauqe, sizeof(opaque));
+    const einsum::EinsumDescriptor &convDescriptor = *einsum::UnpackDescriptor(opauqe, sizeof(opaque));
     std::cout << "conv desc subs: " << convDescriptor.subscripts << "\n";
     std::cout << "conv desc shape: " << convDescriptor.tensor_shape[0] << "\n";
 }

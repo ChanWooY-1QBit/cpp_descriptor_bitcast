b571c579f17dfaa255b5b7962b94b79ea29faa92
ChanWooY-1QBit <chanwoo.yang@1qbit.com>
2022-05-19 10:18:39 -0700

    typo fixed

--- lib/kernel_helpers.h
+++ lib/kernel_helpers.h
@@ -1,10 +1,25 @@
 #include<type_traits>
 #include <iostream>
-#include <pybind11.pybind11.h>
+#include <pybind11/pybind11.h>
 
-#include "absl/base/casts.h"
+// #include "absl/base/casts.h"
 
 namespace einsum {
+    // https://en.cppreference.com/w/cpp/numeric/bit_cast
+    template <class To, class From>
+    typename std::enable_if<sizeof(To) == sizeof(From) && 
+        std::is_trivially_copyable<From>::value && 
+        std::is_trivially_copyable<To>::value, To>::type bit_cast(const From& src) 
+        noexcept {
+            static_assert(
+                std::is_trivially_constructible<To>::value,
+                "This implementation additionally requires destination type to be trivially constructible"
+            );
+            To dst;
+            memcpy(&dst, &src, sizeof(To));
+            return dst;
+    }
+
     template <typename T>
     pybind11::bytes PackDescriptor(const T& descriptor) {
         std::cout << "pybind_helper desc: " << descriptor.subscripts << "\n";
@@ -14,7 +29,7 @@ namespace einsum {
     template <typename T>
     std::string PackDescriptorAsString(const T& descriptor) {
         std::cout << "kernel helpers desc: " << descriptor.subscripts << "\n";
-        return std::string(absl::bit_cast<const char*>(&descriptor), sizeof(T));
+        return std::string(bit_cast<const char*>(&descriptor), sizeof(T));
     }
     
     template <typename T>
@@ -24,6 +39,6 @@ namespace einsum {
         }
         std::cout << "kernel helpers opaque: " << opaque << "\n";
         std::cout << "kernel helpers opaque_len: " << opaque_len << "\n";
-        return absl::bit_cast<const T*>(opaque);
+        return bit_cast<const T*>(opaque);
     }
 }
